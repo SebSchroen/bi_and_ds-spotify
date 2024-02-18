caee313f523f687d230c7f64f2b23fadd48ce187
Sebastian Schroen <S.Schroen@gmx.net>
2024-02-18 09:55:36 +0100

    new column in example data

--- retreive_lyrics.R
+++ retreive_lyrics.R
@@ -69,13 +69,21 @@ lyrics <- foreach(i = 1:nrow(test_sample_songs), .combine = "rbind",
 
 
 saveRDS(lyrics, "rawdata/category_sample_lyrics.RDS")
+
+lyrics <- readRDS("rawdata/category_sample_lyrics.RDS")
 lyrics_unique <- distinct(lyrics)
+
+
+test_sample <- readRDS("rawdata/category_songs.RDS") 
+
 
 final <- test_sample %>% 
   left_join(lyrics_unique, by = c("track.artist", "track.name"))%>% 
   drop_na(sentiment) %>% 
   mutate(track.duration_min = track.duration_ms/60000,
-         words_per_minute = n_words/track.duration_min)
+         words_per_minute = n_words/track.duration_min) %>% 
+  mutate(release_year = year(as_date(track.album.release_date))) %>%
+  select(track.name, track.artist, category, track.duration_min, words_per_minute, sentiment, n_words, lyrics) 
 
 saveRDS(final, "cleandata/example_data_lyrics.RDS")
 
